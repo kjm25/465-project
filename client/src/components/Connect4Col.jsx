01a70a81c3a542a1064527dcc32c128abdf16dd8
Kevin Macdonald <kjm25@cecs.pdx.edu>
2025-03-16 14:15:21 -0700

    client connect4 display working

--- client/src/components/Connect4Col.jsx
+++ client/src/components/Connect4Col.jsx
@@ -1,9 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { socket } from "../socket";
+import "./Connect4Col.css";
 
-function Connect4Col({ col, data }) {
+function Connect4Col({ col, data, turn }) {
   useEffect(() => {}, []);
 
-  return <div className=""></div>;
+  return (
+    <>
+      <div className="d-flex flex-column">
+        <button
+          className={`btn ${turn ? "disabled btn-dark" : "btn-primary"}`}
+          onClick={() => socket.emit("Connect4Drop", col)}
+        >
+          Drop
+        </button>
+        <Connect4Cell colorValue={data[0]} />
+        <Connect4Cell colorValue={data[1]} />
+        <Connect4Cell colorValue={data[2]} />
+        <Connect4Cell colorValue={data[3]} />
+        <Connect4Cell colorValue={data[4]} />
+        <Connect4Cell colorValue={data[5]} />
+      </div>
+    </>
+  );
+}
+
+function Connect4Cell({ colorValue }) {
+  const [color, setColor] = useState("");
+
+  useEffect(() => {
+    if (colorValue === 0) setColor("white");
+    else if (colorValue === 1) setColor("red");
+    else if (colorValue === 2) setColor("yellow");
+  }, [colorValue]);
+  return (
+    <>
+      <div className="outer-cell">
+        <div className={`cell ${color}`}></div>
+      </div>
+    </>
+  );
 }
 
 export default Connect4Col;
--- client/src/pages/Connect4.jsx
+++ client/src/pages/Connect4.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Connect4Col from "../components/Connect4Col";
 
 function Connect4() {
   const startingData = [
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2],
+    [0, 0, 0, 2, 0, 0],
+    [0, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
   ];
-  const [data, setData] = UseState(startingData);
+  const [data, setData] = useState(startingData);
+  const [turn, setTurn] = useState(true); //change to false
+  const [winner, setWinner] = useState("");
 
   return (
     <>
-      <Connect4Col col={1} data={data[0]} />
-      <Connect4Col col={2} data={data[1]} />
-      <Connect4Col col={3} data={data[2]} />
-      <Connect4Col col={4} data={data[3]} />
-      <Connect4Col col={5} data={data[4]} />
-      <Connect4Col col={6} data={data[5]} />
-      <Connect4Col col={7} data={data[6]} />
+      <main className="d-flex justify-content-center">
+        <div className="d-flex">
+          <Connect4Col col={1} data={data[0]} turn={turn} />
+          <Connect4Col col={2} data={data[1]} turn={turn} />
+          <Connect4Col col={3} data={data[2]} turn={turn} />
+          <Connect4Col col={4} data={data[3]} turn={turn} />
+          <Connect4Col col={5} data={data[4]} turn={turn} />
+          <Connect4Col col={6} data={data[5]} turn={turn} />
+          <Connect4Col col={7} data={data[6]} turn={turn} />
+        </div>
+      </main>
     </>
   );
 }
